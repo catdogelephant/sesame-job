70569e0166a9ea049dc1acea3329621544d04b2b
sunxiao <13964402805@36kr.com>
2022-04-15 14:17:19 +0800

    集成Eureka成功

--- src/main/java/com/zhumuchang/dongqu/SesameJobApplication.java
+++ src/main/java/com/zhumuchang/dongqu/SesameJobApplication.java
@@ -3,7 +3,9 @@ package com.zhumuchang.dongqu;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @Slf4j
 @SpringBootApplication
 public class SesameJobApplication {
